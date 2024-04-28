13f5416753f7bf03fc87b03f4f7088c1dd689415
IssaWario <166951562+IssaWario@users.noreply.github.com>
2024-04-28 15:42:00 -0500

    Update Index.php

--- Index.php
+++ Index.php
@@ -281,40 +281,7 @@
 
 
         <!--Footer Code-->
-        <footer>
-            <img src="Images/McNeeseLogo.png" alt="McNeese" id="mcneeselogofooter"> <!--McNeese logo-->
-
-            <div class="socialMedia">
-                <!--Social media logos that bring you to the social media accounts when clicked on-->
-                <ul>
-                    <li><a href="https://www.facebook.com/McNeeseStateU/" target="_blank"><img src="Images/facebook.png" alt="Facebook"></a></li>
-                    <li><a href="https://twitter.com/mcneese" target="_blank"><img src="Images/twitter.png" alt="Twitter"></a></li>
-                    <li><a href="https://www.youtube.com/user/McNeeseStateU" target="_blank"><img src="Images/youtube.png" alt="Youtube"></a></li>
-                    <li><a href="https://www.instagram.com/mcneese/" target="_blank"><img src="Images/instagram.png" alt="Instagram"></a></li>
-                    <li><a href="https://www.linkedin.com/school/27461/" target="_blank"><img src="Images/linkedin.png" alt="LinkedIn"></a></li>
-                </ul>
-            </div>
-
-            <div class="footer">
-                <!--Text that displays the McNeese bookstore hours-->
-                <div class="storeHours">
-                    <h3>Store Hours</h3>
-                    <p>Monday - Thursday: 7:30 AM - 5 PM<br>Friday: 7:30 AM - 11:30 AM</p>
-                </div>
-
-                <!--Text that displays McNeese's address and phone number-->
-                <div class="addressPhonenumber">
-                    <p>McNeese State University | 4205 Ryan St, Lake Charles, LA 70605 | 800-622-3352</p>
-                    <p>&copy; Copyright 2024 TechTitans. All rights reserved.</p>
-                </div>
-
-                <!--Contact us text-->
-                <div class="contactUs">
-                    <h3>Contact Us</h3>
-                    <p>Need to contact us? Please don't hesitate to use the Contact Form.</p>
-                </div>
-            </div>
-        </footer>
+        <?php include("Footer.php") ?>
         <script src="store.js" defer></script>
     </body>
 
