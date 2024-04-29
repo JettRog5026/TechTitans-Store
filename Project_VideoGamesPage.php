3a7c87e9293da6174503d583c327c8ec4dd485c1
IssaWario <166951562+IssaWario@users.noreply.github.com>
2024-04-28 20:52:01 -0500

    Update Project_VideoGamesPage.php

--- Project_VideoGamesPage.php
+++ Project_VideoGamesPage.php
@@ -1,12 +1,11 @@
+<!--
+    TechTitans Store Video Games Page
+    Date last modified: 4/28/2024
+    Who last modified: Morgan Leger
+-->
 <?php
 	include("connectdatabase.php")
 ?>
-<!--
-    TechTitans Store Video Games Page
-    Date last modified: 4/13/2024
-    Who last modified: Morgan Leger
--->
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,15 +29,57 @@
 			{
 				display: none;
 			}
+
+            .Hotcontainer h2{
+                display:block;
+                width:100%
+            }
 		</style>
     </header>
 
      <body>
 		<?php include("Header.php") ?>
-		<?php include("showListing.php") ?>
+		<?php
+            include("connectdatabase.php");
+            $videogames = array();
+            $sql_videogames = "SELECT name,developer,genre,price,picturepath FROM videogames WHERE userAdd = FALSE ORDER BY genre";
+            $result_videogames = mysqli_query($conn, $sql_videogames);
+            if (mysqli_num_rows($result_videogames) > 0) 
+            {
+                //Keep track of displayed genres
+                $displayedGenres = array();
+                while ($row = mysqli_fetch_assoc($result_videogames))
+                {
+                    //Check if the genre has already been displayed
+                    if (!in_array($row["genre"], $displayedGenres)) 
+                    {
+                        echo '<div class="Hotcontainer">';
+                        echo '<h2>'.$row["genre"].'</h2>';
+                        $displayedGenres[] = $row["genre"];
+                        
+                    }
+                    //Display game details
+                    echo '<div class="book">';
+                        echo '<h2>'.$row["name"].'</h2>';
+                        echo '<h4>'.$row["developer"].'</h4>';
+                        echo '<img src="'.$row["picturepath"].'" alt="'.$row["name"].'">';
+                        echo '<p>Price: $'.$row["price"].'</p>';
+                        echo '<button class="add-to-cart-btn">Add to Cart</button>';
+                    echo '</div>';
+
+                    if (!in_array($row["genre"], $displayedGenres) && $row != end($videogames)) 
+                    {
+                        echo '</div>';
+                    }
+                }
+            } 
+            else 
+            {
+                echo "<h1>NO USER INPUTS</h1>";
+            }
+        ?>
 		<?php include("Footer.php") ?>
 	</body>
 	<script src="store.js" defer></script>
-		
 </html>
 
