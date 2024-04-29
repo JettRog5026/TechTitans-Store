890b315b48f3a124c57b3cab9733aa6980a1d190
Havefunay1 <msu-cbertram@gmail.com>
2024-04-28 23:28:44 -0500

    css

--- Project_ComicBooksPage.php
+++ Project_ComicBooksPage.php
@@ -3,7 +3,6 @@
     Date last modified: 4/13/24
     Who last modified: Morgan Leger
 -->
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,199 +23,49 @@
 		<link href = "style.css" type = "text/css" rel = "stylesheet">
     </header>
 
-     <body>
+    <body>
 
-        <?php include("Header.php") ?>
-		
-	<div id="Comic Books" sytle="display: none;">
-
-            <h1>Hot Deals</h1>
-            <div class="Hotcontainer">
-                <div class="book">
-                    <h2>Saga Book Set</h2>
-                    <h4>BrianK. Vaughan / Fiona Staples</h4>
-                    <img src="Images/ComicBooks/saga-box-set.jpg" alt="Saga">
-                    <p>$49.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                    <!-- <p>
-                        Journey across the universe in this critically acclaimed space opera. Saga follows the adventures of Alana and Marko, lovers from warring planets, as they navigate parenthood, interstellar conflict, and bizarre encounters.<br>
-                        This gorgeous box set, you will be collecting all nine of the bestselling trade paperback collections in one affordable package.<br>
-                        It’s the perfect way to introduce any "mature readers" who haven’t yet tried SAGA to our weirdly wonderful universe.
-                    </p><br> -->
-                    <!-- <a href="" id="cart">
-                        <div class="cartbutton">
-                            <button>Add To Cart</button>
-                        </div>
-                    </a> -->
-                </div>
-                <div class="book">
-                    <h2>Ms. Marvel</h2>
-                    <h4>Sana Amanat / Stephen Wacker / G. Willow Wilson / Adrian Alphona</h4>
-                    <img src="Images/ComicBooks/MsMarvel.jpg" alt="Ms Marvel">
-                    <p>$49.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                    <!-- <p>
-                        Meet Kamala Khan, a Pakistani-American teenager from Jersey City who discovers she has shape-shifting abilities.<br>
-                        Follow Kamala's journey as she takes on the mantle of Ms. Marvel and defends her community from threats big and small.
-                    </p> -->
-                </div>
-                <div class="book">
-                    <h2>Invincible Compendium Volume 1</h2>
-                    <h4>Robert Kirkman / Cory Walker / Ryan Ottley</h4>
-                    <img src="Images/ComicBooks/InvincibleV1.jpg" alt="Invincible Collection">
-                    <p>$49.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                    <!-- <p>
-                        The first nine volumes of the greatest superhero comic in the universe<br>
-                        Experience the superhero genre like never before. Invincible follows Mark Grayson, a young hero with inherited powers,<br>
-                        as he navigates the challenges of being a superhero while uncovering dark family secrets.
-                    </p> -->
-                </div>
-
-                <div class="book">
-                    <h2>The Batman Who Laughs</h2>
-                    <h4>Scott Snyder / Jock</h4>
-                    <img src="Images/ComicBooks/batmanLaugh.jpg" alt="Batman Who Laughs">
-                    <p>$49.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                    <!-- <p>
-                        Dive into a twisted tale from the Dark Multiverse. The Batman Who Laughs is a chilling combination of Batman and his arch-nemesis, the Joker.<br>
-                        Follow Batman's struggle against this monstrous new foe in a dark and suspenseful series.
-                    </p> -->
-                </div>
-            </div>
+    <?php include("Header.php") ?>
+    <div id="bookContainer">
+        <?php
+            include("connectdatabase.php");
             
-            <h1>Genres</h1>
-            <h2>Romance</h2>
-            <div class="RomanceContainer">
-                <div class="book">
-                    <h2>The Sun Is Also a Star</h2>
-                    <h4>Nicola Yoon (Author) / David Yoon (Illustrator)</h4>
-                    <img src="Images/ComicBooks/SunisStar.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-                <div class="book">
-                    <h2>Heartstopper</h2>
-                    <h4>Alice Oseman</h4>
-                    <img src="Images/ComicBooks/HeartStop.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-                <div class="book">
-                    <h2>Bloom</h2>
-                    <h4>Kevin Panetta (Author) / Savanna Ganucheau (Illustrator)</h4>
-                    <img src="Images/ComicBooks/Bloom.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-                <div class="book">
-                    <h2>Laura Dean Keeps Breaking Up with Me</h2>
-                    <h4>Mariko Tamaki (Author) / Rosemary Valero-O'Connell (Illustrator)</h4>
-                    <img src="Images/ComicBooks/LauraDean.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-            </div>
-            <h2>Horror</h2>
-            <div class="HorrorContainer">
-                <div class="book">
-                    <h2>Locke & Key</h2>
-                    <h4>Joe Hill (Author) / Gabriel Rodríguez (Illustrator)</h4>
-                    <img src="Images/ComicBooks/Lock.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-                <div class="book">
-                    <h2>Harrow County</h2>
-                    <h4>Cullen Bunn (Author) / Tyler Crook (Illustrator)</h4>
-                    <img src="Images/ComicBooks/HarrowCounty.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-                <div class="book">
-                    <h2>Gideon Falls</h2>
-                    <h4>Jeff Lemire (Author), Andrea Sorrentino (Illustrator)</h4>
-                    <img src="Images/ComicBooks/Gideon.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-                <div class="book">
-                    <h2>Something is Killing the Children</h2>
-                    <h4>James Tynion IV (Author), Werther Dell'Edera (Illustrator)</h4>
-                    <img src="Images/ComicBooks/KillChildren.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-            </div>
-            <h2>Manga</h2>
-            <div class="MangaContainer">
-                <div class="book">
-                    <h2>My Hero Academia</h2>
-                    <h4>Kohei Horikoshi</h4>
-                    <img src="Images/ComicBooks/MyHeroA.png">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-                <div class="book">
-                    <h2>Attack on Titan</h2>
-                    <h4>Hajime Isayama</h4>
-                    <img src="Images/ComicBooks/AttackOnTitan.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-                <div class="book">
-                    <h2>Fruits Basket</h2>
-                    <h4>Natsuki Takaya</h4>
-                    <img src="Images/ComicBooks/FruitsBasket.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-                <div class="book">
-                    <h2>Death Note</h2>
-                    <h4>Tsugumi Ohba (Author) / Takeshi Obata (Illustrator)</h4>
-                    <img src="Images/ComicBooks/DeathNote.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-            </div>
-            <h2>SuperHero</h2>
-            <div class="SuperheroContainer">
-                <div class="book">
-                    <h2>Batman: Hush</h2>
-                    <h4>Jeph Loeb (Author) / Jim Lee (Illustrator)</h4>
-                    <img src="Images/ComicBooks/BatmanHush.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-                <div class="book">
-                    <h2>Ms. Marvel</h2>
-                    <h4>Sana Amanat / Stephen Wacker / G. Willow Wilson / Adrian Alphona</h4>
-                    <img src="Images/ComicBooks/MsMarvel.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-                <div class="book">
-                    <h2>Superman: Red Son</h2>
-                    <h4>Mark Millar (Author) / Dave Johnson (Illustrator)</h4>
-                    <img src="Images/ComicBooks/SupermanRed.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-                <div class="book">
-                    <h2>Spider-Man: Into the Spider-Verse</h2>
-                    <h4>Ralph Macchio</h4>
-                    <img src="Images/ComicBooks/SpiderManSpiderVerse.jpg">
-                    <p>$59.99</p>
-                    <button class="add-to-cart-btn">Add to Cart</button>
-                </div>
-            </div>
+            // SQL query to retrieve books data
+            $sql = "SELECT name, author, genre, price, picturepath, description FROM comicbooks";
+            $result = mysqli_query($conn, $sql);
+            
+            // Close database connection
+            $conn->close();
 
-
-        </div>
-		
+            // Output books as HTML
+            if ($result->num_rows > 0) {
+                $lasGenre = null;
+                while ($row = $result->fetch_assoc()) {
+                    if($row['genre'] != $lasGenre) {
+                        if($lasGenre == null)
+                        {
+                            echo '</div>';
+                        }
+                        echo '<h1>'. $row['genre'] . '</h1>';
+                        echo '<div class="'. $row['genre']. 'container">';
+                        $lasGenre = $row['genre'];
+                    }
+                    echo '<div class="book">';
+                    echo '<h2>' . $row['name'] . '</h2>';
+                    echo '<p>Author: ' . $row['author'] . '</p>';
+                    echo '<img src="' . $row['picturepath'] . '" alt="Book Cover">';
+                    echo '<p>$' . $row['price'] . '</p>';
+                    echo '<p class="description">' . $row['description'] . '</p>';
+                    echo '<button class="add-to-cart-btn">Add to Cart</button>';
+                    echo '</div>';
+                }
+            } else {
+                echo '<p>No books found.</p>';
+            }
+        ?>
+    </div>	
 	<?php include("Footer.php") ?>
-        <script src="navFunctions.js" defer></script>
+    <script src="navFunctions.js" defer></script>
     </body>
 
 </html>
